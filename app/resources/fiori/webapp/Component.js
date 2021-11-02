ab87e3ad949f42414dc1be9d1255f577cd98409a
Vera Lazouskaya <wlazovskaya@gmail.com>
2021-11-02 08:15:01 +0000

    Add project files

---
+++ app/resources/fiori/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/fe/core/AppComponent'], function (AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("testingsaasappscap-fiori.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
